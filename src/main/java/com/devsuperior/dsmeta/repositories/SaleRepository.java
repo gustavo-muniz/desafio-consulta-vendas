facdb9987958e15274240a8ca76853923ef3fe35
Gustavo Muniz <gustamunizbr@gmail.com>
2024-07-25 13:45:39 -0300

    feat: adds Sale report

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,22 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query(value = "SELECT obj " +
+            "FROM Sale obj " +
+            "JOIN FETCH obj.seller " +
+            "WHERE UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%')) " +
+            "AND obj.date BETWEEN :minDate AND :maxDate",
+            countQuery = "SELECT COUNT(obj) FROM Sale obj JOIN obj.seller")
+    Page<Sale> getReport(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,15 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +26,14 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> getReport(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate maxDateReport = maxDate.isBlank() ? LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault()) : LocalDate.parse(maxDate);
+		LocalDate minDateReport = minDate.isBlank() ? maxDateReport.minusYears(1) : LocalDate.parse(minDate);
+
+		Page<Sale> result = repository.getReport(minDateReport, maxDateReport, name, pageable);
+
+		return result.map(SaleReportDTO::new);
 	}
 }
